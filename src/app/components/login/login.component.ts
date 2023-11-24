464f707a80399e939fe1295288d21dc589ed5007
Iurii Armas <iurii.armas@accesa.eu>
2023-11-24 18:58:34 +0200

    feat(login): connect app to firebase

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,7 +1,8 @@
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Component, inject } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { ApiDataService } from 'src/app/api-data.service';
-
 
 @Component({
   selector: 'app-login',
@@ -11,28 +12,46 @@ import { ApiDataService } from 'src/app/api-data.service';
 export class LoginComponent {
   email: string = 'user@example.com';
   password: string = 'password';
+  private firebaseApiUrl = 'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyBYUxdj9AE5EldG9xN36eRrca5Ogiy0zNE';
+
 
   #router = inject(Router)
   #apiService = inject(ApiDataService)
+  #httpClient = inject(HttpClient)
 
-  onLoginSubmit() {
-    // Implement your login logic here
-    if (this.email === 'user@example.com' && this.password === 'password') {
 
-    } else {
-      // Handle login failure
-    }
-    this.#apiService.login(this.email, this.password).subscribe(
-      (response) => {
-        console.log('Handle the successful response here')
-        this.#router.navigate([''])
+  loginForm: FormGroup = new FormGroup({
+    email: new FormControl('', [Validators.required, Validators.email]),
+    password: new FormControl('', [Validators.required, Validators.minLength(5)])
+  });
 
-      },
-      (error) => {
-        console.log('Handle login error here');
-        
-      }
-    );
-  
+  onLoginSubmit(loginForm: FormGroup) {
+    const { email, password } = loginForm.value;
+
+    const params = new HttpParams()
+      .set('email', email)
+      .set('password', password)
+      .set('returnSecureToken', 'true');
+
+    this.#httpClient
+      .post(this.firebaseApiUrl, null, { params })
+      .subscribe(
+        (response: any) => {
+          // User authenticated successfully
+          const token = response.idToken;
+          console.log(token);
+
+          sessionStorage.setItem('token', token)
+
+          // Save the token in localStorage or a secure storage
+          localStorage.setItem('authToken', token);
+
+          // Redirect to home or desired page
+          this.#router.navigate(['/']);
+        },
+        (error) => {
+          console.log(error);
+        }
+      );
   }
 }
